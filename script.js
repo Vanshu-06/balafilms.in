b3a410c2bb873b96bc5e070bfcd9affad4f8b289
vansh <singhvanshpreet79@gmail.com>
2026-06-07 16:19:17 +0530

    image added

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
+// FAQ
 
-        document.querySelector(
-            this.getAttribute('href')
-        ).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+const question = item.querySelector(".question");
+
+question.addEventListener("click", () => {
+
+item.classList.toggle("active");
+
+});
+
+});
+
+// Navbar Shadow
+
+window.addEventListener("scroll", () => {
+
+const header = document.querySelector("header");
+
+if(window.scrollY > 50){
+header.style.boxShadow =
+"0 4px 15px rgba(0,0,0,.15)";
+}else{
+header.style.boxShadow =
+"0 2px 10px rgba(0,0,0,.08)";
+}
+
+});
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]')
+.forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(
+this.getAttribute("href")
+).scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
 });
